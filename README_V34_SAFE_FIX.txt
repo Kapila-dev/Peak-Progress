680d3efbd8eb7c6fb65722a32eebc46c74e0e3a7
Kapila-dev <zielinskikap@gmail.com>
2026-04-27 18:25:48 -0500

    Add files via upload

---
+++ README_V34_SAFE_FIX.txt
@@ -0,0 +1,1 @@
+v34 safe fix: repairs v33 syntax issue, keeps adaptive builder update, bumps cache/version. Upload all root files to GitHub Pages.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'peak-progress-v33';
+const CACHE_NAME = 'peak-progress-v34';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
